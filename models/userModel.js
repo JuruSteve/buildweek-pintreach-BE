733148bd814f28f705331a810ceabf595f5adfd8
korynewton <korynewtondev@gmail.com>
2019-04-15 10:29:44 -0700

    add() updated, returns user object upon successful registration

--- models/userModel.js
+++ models/userModel.js
@@ -2,13 +2,18 @@ const db = require('../data/dbConfig.js');
 
 module.exports = {
   add,
-  findBy,
 };
 
-function add() {
-  return;
+async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return db('users')
+    .where({ id })
+    .first();
 }
 
-function findBy(filter) {
-  return;
-}
+// function findBy(filter) {
+//   return db('users')
+//     .where({ id })
+//     .first();
+// }
--- routes/auth/authRouter.js
+++ routes/auth/authRouter.js
@@ -7,7 +7,7 @@ const Auth = require('../../models/userModel');
 router.post('/register', async (req, res) => {
   let user = req.body;
 
-  if (!user.username || !user.password) {
+  if (!user.username || !user.password || !user.name || !user.email) {
     return res
       .status(406)
       .json({ message: 'Must include a username and password to register' });
@@ -15,12 +15,19 @@ router.post('/register', async (req, res) => {
 
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
+
   try {
     const register = await Auth.add(user);
     if (register) {
-      res.status(201).json(register);
+      res
+        .status(201)
+        .json(register)
+        .end();
     } else {
-      res.status(400).json({ message: 'user already registered' });
+      res
+        .status(400)
+        .json({ message: 'user already registered' })
+        .end();
     }
   } catch (error) {
     res.status(500).json(error);
