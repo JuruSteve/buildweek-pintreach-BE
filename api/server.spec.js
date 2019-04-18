9dc8a3620e2864db7f3106523c6bc22c2c3a80b8
korynewton <korynewtondev@gmail.com>
2019-04-17 20:02:37 -0700

    server testing

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,8 +2,18 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server.js', () => {
-  it('should respond 200 OK status', async () => {
-    const res = await request(server).get('/');
-    expect(res.status).toBe(200);
+  describe('GET / to base url', () => {
+    it('should respond 200 OK status', async () => {
+      const res = await request(server).get('/');
+      expect(res.status).toBe(200);
+    });
+    it('should return json object', async () => {
+      const res = await request(server).get('/');
+      expect(res.type).toBe('application/json');
+    });
+    it("should return { message: 'sanity check passed!' }", async () => {
+      const res = await request(server).get('/');
+      expect(res.body).toEqual({ message: 'sanity check passed!' });
+    });
   });
 });
